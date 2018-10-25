5ee89551510c8de631f08980abcc0bf300b170a9
Carrie Li <carrie.t.li@gmail.com>
2018-10-25 03:50:17 +0000

    Joined dataframes

--- product-sales.py
+++ product-sales.py
@@ -44,15 +44,83 @@ class ProductSales:
         if not self.df:
             try:
                 products_df = pd.read_csv(self.products_url, sep=',', \
-                                            names = ['id', 'prod_name'])
+                                            names = ['ID', 'Prod_Name'])
+
                 sales_df = pd.read_csv(self.sales_url, sep=',', \
                                         infer_datetime_format=True, \
-                                        names = ['id', 'date', 'price', 'quantity'])
+                                        names = ['ID', 'Date', 'Price', 'Quantity'])
+
+                # Left join the two dfs on 'ID', which will be set as the index
+                df = products_df.set_index('ID').join(sales_df.set_index('ID'))
 
             except:
                 print("Error: No data found!")
 
-        return sales_df
+        return df
+
+#                                   Prod_Name        Date  Price  Quantity
+# ID
+# 1                             Windswell IPA  2017-01-11  13.99         1
+# 1                             Windswell IPA  2017-03-09  13.99         1
+# 1                             Windswell IPA  2017-03-11  13.99         1
+# 1                             Windswell IPA  2017-02-05  13.99         1
+# 1                             Windswell IPA  2017-01-30  13.99         2
+# 1                             Windswell IPA  2017-01-09  13.99         2
+# 1                             Windswell IPA  2017-02-14  13.99         2
+# 1                             Windswell IPA  2017-03-07  13.99         2
+# 1                             Windswell IPA  2017-03-02  13.99         2
+# 1                             Windswell IPA  2017-03-19  13.99         1
+# 1                             Windswell IPA  2017-03-25  13.99         2
+# 1                             Windswell IPA  2017-03-17  13.99         2
+# 1                             Windswell IPA  2017-01-02  13.99         2
+# 1                             Windswell IPA  2017-02-11  13.99         1
+# 1                             Windswell IPA  2017-03-26  13.99         5
+# 1                             Windswell IPA  2017-01-05  13.99         1
+# 1                             Windswell IPA  2017-03-23  13.99         1
+# 1                             Windswell IPA  2017-02-10  13.99         3
+# 1                             Windswell IPA  2017-03-17  13.99         1
+# 1                             Windswell IPA  2017-01-15  13.99         6
+# 1                             Windswell IPA  2017-01-24  13.99         1
+# 1                             Windswell IPA  2017-03-30  13.99         2
+# 1                             Windswell IPA  2017-03-12  13.99         2
+# 1                             Windswell IPA  2017-03-05  13.99         8
+# 1                             Windswell IPA  2017-01-23  13.99         2
+# 1                             Windswell IPA  2017-03-28  13.99         2
+# 1                             Windswell IPA  2017-02-13  13.99         7
+# 1                             Windswell IPA  2017-01-05  13.99         2
+# 1                             Windswell IPA  2017-03-27  13.99         2
+# 1                             Windswell IPA  2017-01-27  13.99         5
+# ..                                      ...         ...    ...       ...
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-18   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-24   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-03-01   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-13   1.99         5
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-20   1.99         3
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-12   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-02   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-31   1.99         4
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-10   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-19   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-25   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-05   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-10   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-03-03   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-06   1.99         9
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-03-21   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-03   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-07   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-03-21   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-12   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-24   1.99         5
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-20   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-13   1.99         7
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-03-13   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-23   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-23   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-27   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-02-04   1.99         2
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-05   1.99         1
+# 11  Reese Witherspoon's All-Natural PB Cups  2017-01-16   1.99         1
 
 
 if __name__ == "__main__":
